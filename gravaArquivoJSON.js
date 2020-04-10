c6deff58c6e480181ab893ef868b5df00999f841
codelie <62216257+codelie@users.noreply.github.com>
2020-04-10 14:03:04 -0300

    Add files via upload

--- gravaArquivoJSON.js
+++ gravaArquivoJSON.js
@@ -4,9 +4,11 @@ let conteudoSobrenome = 'de Tal';
 let conteudoSalario = 1983.37;
 
 let conteudoTextoJSON = '{';
-conteudoTextoJSON += '"nome": "' + conteudoNome + '",';
-conteudoTextoJSON += '"sobreNome": "' + conteudoSobrenome + '",';
-conteudoTextoJSON += '"salario": "' + conteudoSalario + '"';
+
+    conteudoTextoJSON += '"nome": "' + conteudoNome + '",';
+    conteudoTextoJSON += '"sobreNome": "' + conteudoSobrenome + '",';
+    conteudoTextoJSON += '"salario": "' + conteudoSalario + '"';
+    
 conteudoTextoJSON += '}';
 
 let blob = new Blob( [conteudoTextoJSON] , {type: "text/plain;charset=utf-8"} );
