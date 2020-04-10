08e69bf9dc231909a7b1e7950a78437f2d3e9dc7
codelie <62216257+codelie@users.noreply.github.com>
2020-04-10 12:16:34 -0300

    Add files via upload

---
+++ gravaArquivoJSON.js
@@ -0,0 +1,13 @@
+let nomeArquivo = 'NotaFiscal';
+let conteudoNome = 'Fulano';
+let conteudoSobrenome = 'de Tal';
+let conteudoSalario = 1983.37;
+
+let conteudoTextoJSON = '{';
+conteudoTextoJSON += '"nome": "' + conteudoNome + '",';
+conteudoTextoJSON += '"sobreNome": "' + conteudoSobrenome + '",';
+conteudoTextoJSON += '"salario": "' + conteudoSalario + '"';
+conteudoTextoJSON += '}';
+
+let blob = new Blob( [conteudoTextoJSON] , {type: "text/plain;charset=utf-8"} );
+saveAs(blob,nomeArquivo+".json");
